4dec2b4871716de106209a74ffca4d00e3c95cbb
Nimish Tiwari <nimish.tiwari@somaiya.edu>
2023-10-20 11:28:26 +0530

    Class Update

--- routes/class.js
+++ routes/class.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const ClassController = require('../controllers/ClassController');
 const adminConstraint = require('../middlewares/adminConstraint');
+const tokenVerify = require('../middlewares/tokenVerify');
 
 router.post('/class/create', adminConstraint, ClassController.createClass);
-router.get('/class/getAll',adminConstraint, ClassController.getAllClasses);
-router.get('/class/get/:id',adminConstraint, ClassController.getClassById);
+router.get('/class/getAll',tokenVerify, ClassController.getAllClasses);
+router.get('/class/get/:id',tokenVerify, ClassController.getClassById);
 router.put('/class/update/:id',adminConstraint, ClassController.updateClass);
 router.delete('/class/delete/:id',adminConstraint, ClassController.deleteClass);
 
