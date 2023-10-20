8ea93b0947cf14ff41539544132dabdd71bec968
Akshit Savaliya <akshitsavaliya999@outlook.com>
2023-10-20 12:02:40 +0530

    .

--- routes/student.js
+++ routes/student.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 const StudentController = require('../controllers/StudentController');
 const adminConstraint = require('../middlewares/adminConstraint');
+const tokenVerify = require('../middlewares/tokenVerify');
 
 
 router.post('/student/create', adminConstraint,StudentController.createStudent);
-router.get('/student/getAll', StudentController.getAllStudents);
+router.get('/student/getAll', tokenVerify, StudentController.getAllStudents);
 router.get('/student/get/:id', StudentController.getStudentById);
 router.put('/student/update/:id', StudentController.updateStudent);
 router.delete('/student/delete/:id', StudentController.deleteStudent);
